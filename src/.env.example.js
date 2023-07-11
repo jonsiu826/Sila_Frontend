c639f4c89d91841494fc99381c019503a71d3513
jonsiu826 <jonsiu826@gmail.com>
2023-07-11 15:23:17 -0700

    adding env example file

---
+++ src/.env.example.js
@@ -0,0 +1,22 @@
+// app information
+const APP_HANDLE = '';
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
+const JWT_TOKEN = ''
+const PLAID_CLIENT_ID = ''
+const PLAID_SECRET = ''
+
+//individual user
+const USER_HANDLE_INDIVIDUAL_ONE = ''
+const USER_HANDLE_INDIVIDUAL_TWO = ''
+
+module.exports = {
+    APP_HANDLE,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    JWT_TOKEN,
+    PLAID_SECRET,
+    PLAID_CLIENT_ID,
+    USER_HANDLE_INDIVIDUAL_ONE,
+    USER_HANDLE_INDIVIDUAL_TWO
+}
